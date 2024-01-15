c9ec515a91f6c89158ce346f1d9e4b88c0e26650
Vincent <Vincentapartgames@gmail.com>
2024-01-15 14:20:55 +0100

    Troubleshooting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -40,32 +40,32 @@
       </div>
     </div>
     <div class="relative col-start-3 col-span-5 group border border-gray-700 bg-gradient-to-br from-gray-800 to-gray-900 rounded-3xl p-8 flex gap-6">
-      <div class="flex flex-col gap-2 justify-center">
-        <div class="uppercase text-sm">
-          Big Five Personality Test.
-        </div>
-        <div class="text-4xl text-white font-semibold">
-          Wat kan ik?
-        </div>
-        <div>
-          Bij deze test word er gekeken naar 5 aspecten van je persoonlijkheid.<br>
-          <bold>Openheid voor ervaringen:</bold><br>
-          Hier is uit gebleken dat ik wat meer traditioneel ben en me nuchter gedraag, ik houd van het praktisch blijven en denken. Wat ik minder doe is dus experimenteel denken, ik ben wat minder fantasierijk dan gemiddeld. Mijn percentage bij deze test was 34%.
-          <br><br>
-          <bold>Gewetensvolheid:</bold><br>
-          Ik ben erg spontaan en houd van flexibel plannen, ik ga liever niet elke dag precies hetzelfde traject door en heb veel afwisseling nodig. Ik ben dus wat minder georganiseerd en efficiënt. Mijn percentage bij deze test was 11%.
-          <br><br>
-          <bold>Extraversie:</bold><br>
-          Ik ben gereserveerd en formeel, ik ben stil en werk het liefst alleen. Ik zit graag niet in een leiders rol en doe het liefst alleen de taken die aan mij gegeven zijn. Mijn percentage bij deze test was 6%
-          <br><br>
-          <bold>Meegaandheid:</bold><br>
-          Ik leef meer met mensen mee dan gemiddeld, zorg dat mensen het naar hun zin hebben en dat ze zich niet rot voelen. Ik ben gedreven naar een oplossing te komen in plaats van concurreren en conflicteren. Mijn percentage bij deze test was 70%
-          <br><br>
-          <bold>Natuurlijke reacties:</bold><br>
-          Met deze uitslag ben ik het minder eens, hier is namelijk uitgekomen dat ik negatieve emotionele reacties zou hebben  en geneigd ben tot piekeren. Zelf denk ik dat dit niet het geval is, ik kan snel mijn emoties aan de kant zetten en verder gaan met iets. Dit komt later dan een keer aan bod als ik wat afgekoeld ben. Mijn percentage bij deze test was 83%
+<!--      <div class="flex flex-col gap-2 justify-center">-->
+<!--        <div class="uppercase text-sm">-->
+<!--          Big Five Personality Test.-->
+<!--        </div>-->
+<!--        <div class="text-4xl text-white font-semibold">-->
+<!--          Wat kan ik?-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          Bij deze test word er gekeken naar 5 aspecten van je persoonlijkheid.<br>-->
+<!--          <bold>Openheid voor ervaringen:</bold><br>-->
+<!--          Hier is uit gebleken dat ik wat meer traditioneel ben en me nuchter gedraag, ik houd van het praktisch blijven en denken. Wat ik minder doe is dus experimenteel denken, ik ben wat minder fantasierijk dan gemiddeld. Mijn percentage bij deze test was 34%.-->
+<!--          <br><br>-->
+<!--          <bold>Gewetensvolheid:</bold><br>-->
+<!--          Ik ben erg spontaan en houd van flexibel plannen, ik ga liever niet elke dag precies hetzelfde traject door en heb veel afwisseling nodig. Ik ben dus wat minder georganiseerd en efficiënt. Mijn percentage bij deze test was 11%.-->
+<!--          <br><br>-->
+<!--          <bold>Extraversie:</bold><br>-->
+<!--          Ik ben gereserveerd en formeel, ik ben stil en werk het liefst alleen. Ik zit graag niet in een leiders rol en doe het liefst alleen de taken die aan mij gegeven zijn. Mijn percentage bij deze test was 6%-->
+<!--          <br><br>-->
+<!--          <bold>Meegaandheid:</bold><br>-->
+<!--          Ik leef meer met mensen mee dan gemiddeld, zorg dat mensen het naar hun zin hebben en dat ze zich niet rot voelen. Ik ben gedreven naar een oplossing te komen in plaats van concurreren en conflicteren. Mijn percentage bij deze test was 70%-->
+<!--          <br><br>-->
+<!--          <bold>Natuurlijke reacties:</bold><br>-->
+<!--          Met deze uitslag ben ik het minder eens, hier is namelijk uitgekomen dat ik negatieve emotionele reacties zou hebben  en geneigd ben tot piekeren. Zelf denk ik dat dit niet het geval is, ik kan snel mijn emoties aan de kant zetten en verder gaan met iets. Dit komt later dan een keer aan bod als ik wat afgekoeld ben. Mijn percentage bij deze test was 83%-->
 
-        </div>
-      </div>
+<!--        </div>-->
+<!--      </div>-->
     </div>
 
   </div>
